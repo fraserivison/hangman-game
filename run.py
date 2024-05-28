5f1a839828a212bdd3b324267da8270cabac317a
fraserivison <fraserivison@hotmail.co.uk>
2024-05-28 15:36:32 +0000

    Add intro to game and dictionary for the words to be used

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
+import random
+
+print("Hi there, welcome to Hangman!")
+print("Guess wisely, every letter counts...")
+
+wordDictionary = {
+    "Animals": ["platypus", "meerkat", "kangaroo", "warthog", "lobster", "leopard", "goose"],
+    "Countries": ["mexico", "nepal", "greenland", "denmark", "slovenia", "scotland", "liechtenstein"],
+    "Food": ["banana", "pickles", "spaghetti", "cheesecake", "olives", "yoghurt", ]
+}
+
+
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
