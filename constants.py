114f02c334dc10cfcfbe5da03d10500ed1d10191
fraserivison <fraserivison@hotmail.co.uk>
2024-05-31 21:46:24 +0000

    Run constants.py through PEP8 validator, use double backslash to solve single backslash issue that had a W605 invalid escape sequence error

--- constants.py
+++ constants.py
@@ -1,60 +1,72 @@
 # Function to print the hangman based on the number of wrong guesses
-# code taken from (https://github.com/ShaunHalverson/PythonHangman/blob/main/main.py)
+# code taken from-
+# (https://github.com/ShaunHalverson/PythonHangman/blob/main/main.py)
 def print_hangman(wrong):
-    if(wrong == 0):
+    if wrong == 0:
         print("")
         print("+---+")
         print("    |")
         print("    |")
         print("    |")
         print("   ===")
-    elif(wrong == 1): 
+    elif wrong == 1:
         print("")
         print("+---+")
         print("O   |")
         print("    |")
         print("    |")
         print("   ===")
-    elif(wrong == 2):
+    elif wrong == 2:
         print("")
         print("+---+")
         print("O   |")
         print("|   |")
         print("    |")
         print("   ===")
-    elif(wrong == 3):
+    elif wrong == 3:
         print("")
         print("+---+")
         print(" O  |")
         print("/|  |")
         print("    |")
         print("   ===")
-    elif(wrong == 4):
+    elif wrong == 4:
         print("")
         print("+---+")
         print(" O  |")
-        print("/|\ |")
+        print("/|\\|")
         print("    |")
         print("   ===")
-    elif(wrong == 5):
+    elif wrong == 5:
         print("")
         print("+---+")
         print(" O  |")
-        print("/|\ |")
+        print("/|\\|")
         print("/   |")
         print("   ===")
-    elif(wrong == 6):
+    elif wrong == 6:
         print("")
         print("+---+")
         print(" O   |")
-        print("/|\  |")
-        print("/ \  |")
+        print("/|\\ |")
+        print("/ \\ |")
         print("    ===")
+
 
 # Dictionary containing word categories
 WORD_DICTIONARY = {
-    "Animals": ["platypus", "meerkat", "kangaroo", "warthog", "lobster", "leopard", "goose"],
-    "Countries": ["mexico", "nepal", "greenland", "denmark", "slovenia", "scotland", "liechtenstein"],
-    "Food": ["banana", "pickles", "spaghetti", "cheesecake", "olives", "yoghurt", ]
+    "Animals": [
+        "platypus", "meerkat", "kangaroo", "warthog", "lobster",
+        "leopard", "goose"
+    ],
+    "Countries": [
+        "mexico", "nepal", "greenland", "denmark", "slovenia",
+        "scotland", "liechtenstein"
+    ],
+    "Food": [
+        "banana", "pickles", "spaghetti", "cheesecake", "olives",
+        "yoghurt",
+    ]
+}
 }
 
